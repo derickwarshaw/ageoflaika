0c4cd129c4798c8b31bbc7d54a07f22a774ae8a4
Derick Warshaw <derickwarshaw@gmail.com>
2015-07-18 16:10:25 -0400

    updated for control flow challenge

--- AgeOfLaika/AgeOfLaika/ViewController.swift
+++ AgeOfLaika/AgeOfLaika/ViewController.swift
@@ -34,5 +34,26 @@ class ViewController: UIViewController {
         
         enterHumanYearsTextField.resignFirstResponder()
     }
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        
+        let stringFromTextField = enterHumanYearsTextField.text
+        let doubleFromTextField = Double((stringFromTextField as NSString).doubleValue)
+        
+        var realDogYears:Double
+        
+        if doubleFromTextField > 2 {
+            realDogYears = (10.5 * 2) + (doubleFromTextField - 2 * 4)
+        } else {
+            realDogYears = doubleFromTextField * 10.5
+        }
+        
+        dogYearsLabel.hidden = false
+        
+        dogYearsLabel.text="\(realDogYears)" + " Real Human Years"
+        
+        enterHumanYearsTextField.resignFirstResponder()
+        
+        
+    }
 }
 
